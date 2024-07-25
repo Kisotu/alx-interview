82fe285ec5afd4f8336431b1d675d793cc09d09e
Oloishorua <jacoboloishorua@gmail.com>
2024-07-25 21:49:48 +0300

    pascal triangle requirement fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,9 +13,13 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1 for _ in range(i+1)] for i in range(n)]
-    for i in range(2, n):
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
         for j in range(1, i):
-            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
 
     return triangle
