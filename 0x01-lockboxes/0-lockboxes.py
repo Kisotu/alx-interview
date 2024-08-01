1d3bde6796c9a8a76930bb72eae03987438b9088
Oloishorua <jacoboloishorua@gmail.com>
2024-08-01 22:03:04 +0300

    solution to det if a box can be opened

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -14,14 +14,14 @@ def canUnlockAll(boxes):
     Returns:
         bool: True if all boxes can be unlocked, False otherwise.
     """
-    n = len(boxes)
-    seen_boxes = set([0])
-    unseen_boxes = set(boxes[0]).difference(set([0]))
-    while len(unseen_boxes) > 0:
-        boxIdx = unseen_boxes.pop()
-        if not boxIdx or boxIdx >= n or boxIdx < 0:
+    leng = len(boxes)
+    visit_boxes = set([0])
+    unvisited_boxes = set(boxes[0]).difference(set([0]))
+    while len(unvisited_boxes) > 0:
+        boxIndex = unvisited_boxes.pop()
+        if not boxIndex or boxIndex >= leng or boxIndex < 0:
             continue
-        if boxIdx not in seen_boxes:
-            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
-            seen_boxes.add(boxIdx)
-    return n == len(seen_boxes)
+        if boxIndex not in visit_boxes:
+            unvisited_boxes = unvisited_boxes.union(boxes[boxIndex])
+            visit_boxes.add(boxIndex)
+    return leng == len(visit_boxes)
