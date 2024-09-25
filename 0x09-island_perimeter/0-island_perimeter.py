3be0264e0206f82555e7a2589c912e66f994d963
Oloishorua <jacoboloishorua@gmail.com>
2024-09-25 20:06:01 +0300

    task 0  - computes the perimeter of an island described in the grid

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Island perimeter Task
+"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of an island described in grid
+    """
+    perim = 0
+    if type(grid) is not list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perim += sum(edges)
+    return perim
